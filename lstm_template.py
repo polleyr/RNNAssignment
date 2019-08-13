85e2e21d9133dcf64bb04068794837da4840fcfe
Rupert Polley <Rupert.Polley@gmx.de>
2019-08-13 15:19:08 +0200

    solved assignement

--- lstm_template.py
+++ lstm_template.py
@@ -2,9 +2,12 @@
 Minimal character-level LSTM model. Written by Ngoc Quan Pham
 Code structure borrowed from the Vanilla RNN model from Andreij Karparthy @karparthy.
 BSD License
+
+Adapted by R and N
 """
 import numpy as np
 from random import uniform
+from random import random
 import sys
 
 
@@ -29,7 +32,6 @@ def softmax(x):
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum()
 
-
 # data I/O
 data = open('data/input.txt', 'r').read() # should be simple plain text file
 chars = list(set(data))
@@ -43,7 +45,7 @@ option = sys.argv[1]
 
 # hyperparameters
 emb_size = 4
-hidden_size = 32  # size of hidden layer of neurons
+hidden_size = 200  # size of hidden layer of neurons
 seq_length = 64  # number of steps to unroll the RNN for
 learning_rate = 5e-2
 max_updates = 500000
@@ -56,6 +58,8 @@ Wex = np.random.randn(emb_size, vocab_size)*std # embedding layer
 
 # LSTM parameters
 Wf = np.random.randn(hidden_size, concat_size) * std # forget gate
+
+
 Wi = np.random.randn(hidden_size, concat_size) * std # input gate
 Wo = np.random.randn(hidden_size, concat_size) * std # output gate
 Wc = np.random.randn(hidden_size, concat_size) * std # c term
@@ -83,7 +87,7 @@ def forward(inputs, targets, memory):
 
     # Here you should allocate some variables to store the activations during forward
     # One of them here is to store the hiddens and the cells
-    hs, cs = {}
+    hs, cs, xs, zs, wes, os, ps, ys, f_gate, i_gate, hat_c, o_gate = {},{},{},{},{},{},{},{},{},{},{},{}
 
     hs[-1] = np.copy(hprev)
     cs[-1] = np.copy(cprev)
@@ -102,50 +106,57 @@ def forward(inputs, targets, memory):
         # This step is irregular (to save the amount of matrix multiplication we have to do)
         # I will refer to this vector as [h X]
         zs[t] = np.row_stack((hs[t-1], wes[t]))
-
         # YOUR IMPLEMENTATION should begin from here
 
         # compute the forget gate
-        # f_gate = sigmoid (W_f \cdot [h X] + b_f)
+        f_gate[t] = sigmoid (np.dot(Wf, zs[t]) + bf)
 
         # compute the input gate
-        # i_gate = sigmoid (W_i \cdot [h X] + b_i)
+        i_gate[t] = sigmoid (np.dot(Wi, zs[t]) + bi)
 
         # compute the candidate memory
-        # \hat{c} = tanh (W_c \cdot [h X] + b_c])
+        hat_c[t] = np.tanh(np.dot(Wc, zs[t]) + bc)
 
         # new memory: applying forget gate on the previous memory
         # and then adding the input gate on the candidate memory
-        # c_new = f_gate * prev_c + i_gate * \hat{c}
+
+        cs[t] = f_gate[t] * cs[t-1] + i_gate[t] * hat_c[t]
 
         # output gate
-        # o_gate = sigmoid (Wo \cdot [h X] + b_o)
+        o_gate[t] = sigmoid (np.dot(Wo, zs[t]) + bo)
 
         # new hidden state for the LSTM
-        # h = o_gate * tanh(c_new)
+        hs[t] = o_gate[t] * np.tanh(cs[t])
 
         # DONE LSTM
         # output layer - softmax and cross-entropy loss
         # unnormalized log probabilities for next chars
 
-        # o = Why \cdot h + by
+        os[t] = np.dot(Why, hs[t]) + by
 
         # softmax for probabilities for next chars
-        # p = softmax(o)
+        ps[t] = softmax(os[t])
 
         # cross-entropy loss
         # cross entropy loss at time t:
         # create an one hot vector for the label y
-
+        ys[t] = np.zeros((vocab_size, 1))
+        ys[t][targets[t]] = 1
         # and then cross-entropy (see the elman-rnn file for the hint)
+        loss_t = np.sum(-np.log(ps[t]) * ys[t])
+        loss += loss_t
 
     # define your activations
+
+    activations = (hs, cs, xs, zs, wes, os, ps, ys, f_gate, i_gate, hat_c, o_gate)
+
     memory = (hs[len(inputs)-1], cs[len(inputs)-1])
 
     return loss, activations, memory
 
 
 def backward(activations, clipping=True):
+    hs, cs, xs, zs, wes, os, ps, ys, f_gate, i_gate, hat_c, o_gate = activations
 
     # backward pass: compute gradients going backwards
     # Here we allocate memory for the gradients
@@ -156,15 +167,62 @@ def backward(activations, clipping=True):
 
     # similar to the hidden states in the vanilla RNN
     # We need to initialize the gradients for these variables
-    dhnext = np.zeros_like(hs[0])
-    dcnext = np.zeros_like(cs[0])
+    dh = np.zeros_like(hs[0])
+    dc = np.zeros_like(cs[0])
 
     # back propagation through time starts here
     for t in reversed(range(len(inputs))):
-
         # IMPLEMENT YOUR BACKPROP HERE
         # refer to the file elman_rnn.py for more details
 
+        do = ps[t] - ys[t]
+        dWhy += np.dot(do, hs[t].T)
+        dby += do
+        dh = np.dot(Why.T, do) + dh  # add dh from previous lstm cell
+
+        # output gate
+        do_gate = dh * np.tanh(cs[t])
+        d_sig_ogate = o_gate[t] * (1 - o_gate[t])
+        do_gate_pre = d_sig_ogate * do_gate
+        dbo += do_gate_pre
+        dWo += np.dot(do_gate_pre, zs[t].T)
+        dzs = np.dot(Wo.T, do_gate_pre) # dzs gets values from 4 different paths
+
+        # derivative from cell state, add cell state from previous lstm cell
+        # CAUTION, dtanh only does 1- x^2!! So use np.tanh(cs[t]) here!
+        dc = dh * o_gate[t] * dtanh(np.tanh(cs[t])) + dc
+
+        # hat_c gate
+        dhat_c = dc * i_gate[t]
+        dtanh_hat_c = dtanh(hat_c[t])
+        dhat_c_pre = dtanh_hat_c * dhat_c
+        dWc += np.dot(dhat_c_pre, zs[t].T)
+        dbc += dhat_c_pre
+        dzs += np.dot(Wc.T, dhat_c_pre) # -> add to gradient from output gate
+
+        # input gate
+        di_gate = dc * hat_c[t]
+        dsig_i_gate = dsigmoid(i_gate[t])
+        di_gate_pre = dsig_i_gate * di_gate
+        dWi += np.dot(di_gate_pre, zs[t].T)
+        dbi += di_gate_pre
+        dzs += np.dot(Wi.T, di_gate_pre) # sum gradients
+
+        # forget gate
+        df_gate = dc * cs[t-1]
+        dsig_f_gate = dsigmoid(f_gate[t])
+        df_gate_pre = dsig_f_gate * df_gate
+        dWf += np.dot(df_gate_pre, zs[t].T)
+        dbf += df_gate_pre
+        dzs += np.dot(Wf.T, df_gate_pre) # sum gradients
+
+
+        dc = dc * f_gate[t] # set local variable for previous cell
+        dh = dzs[0:hidden_size] # set for next cell, zs concats h and wes
+
+
+        dwes = dzs[hidden_size:hidden_size + emb_size] # cause zs is concated
+        dWex += np.dot(dwes, xs[t].T)
 
     if clipping:
         # clip to mitigate exploding gradients
@@ -184,13 +242,42 @@ def sample(memory, seed_ix, n):
     h, c = memory
     x = np.zeros((vocab_size, 1))
     x[seed_ix] = 1
+    generated_chars = []
 
     for t in range(n):
         # IMPLEMENT THE FORWARD FUNCTION ONE MORE TIME HERE
         # BUT YOU DON"T NEED TO STORE THE ACTIVATIONS
 
+        wes = np.dot(Wex, x)
+        zs =  np.concatenate((h, wes), axis = 0)
+        f_gate = sigmoid(np.dot(Wf, zs) + bf)
+        i_gate = sigmoid(np.dot(Wi, zs) + bi)
+        hat_c = np.tanh(np.dot(Wc, zs) + bc)
 
-    return
+        c = c * f_gate + i_gate*hat_c
+        output_gate = sigmoid(np.dot(Wo, zs) + bo)
+        h = output_gate * np.tanh(c)
+
+        os = np.dot(Why, h) + by
+
+        # softmax for probabilities for next chars
+        p = softmax(os)
+
+        x = np.zeros((vocab_size, 1))
+
+        if (random() < 0.33): # 33% chance to use the argmax
+            argmaxIndex = np.argmax(p)
+            ix = np.zeros((vocab_size, 1))
+            ix[argmaxIndex] = 1
+        else:
+            ix = np.random.multinomial(1, p.ravel())
+        for j in range(len(ix)):
+            if ix[j] == 1:
+                index = j
+
+        x[index] = 1
+        generated_chars.append(index)
+    return generated_chars
 
 if option == 'train':
 
@@ -260,14 +347,20 @@ elif option == 'gradcheck':
     gradients = backward(activations, clipping=False)
     dWex, dWf, dWi, dWo, dWc, dbf, dbi, dbo, dbc, dWhy, dby = gradients
 
+    np.set_printoptions(precision = 7)
+    errorSum = 0
+    weightSum = 0
     for weight, grad, name in zip([Wf, Wi, Wo, Wc, bf, bi, bo, bc, Wex, Why, by], 
-                                   [dWf, dWi, dWo, dWc, dbf, dbi, dbo, dbc, dWex    , dWhy, dby],
+                                   [dWf, dWi, dWo, dWc, dbf, dbi, dbo, dbc, dWex, dWhy, dby],
                                    ['Wf', 'Wi', 'Wo', 'Wc', 'bf', 'bi', 'bo', 'bc', 'Wex', 'Why', 'by']):
 
         str_ = ("Dimensions dont match between weight and gradient %s and %s." % (weight.shape, grad.shape))
         assert(weight.shape == grad.shape), str_
 
-        print(name)
+        zeroDivCounter = 0
+
+        print(name,  " has ", weight.size, " individual weights")
+        weightSum += weight.size
         for i in range(weight.size):
       
             # evaluate cost at [x + delta] and [x - delta]
@@ -282,7 +375,15 @@ elif option == 'gradcheck':
             grad_numerical = (loss_positive - loss_negative) / ( 2 * delta )
 
             # compare the relative error between analytical and numerical gradients
-            rel_error = abs(grad_analytic - grad_numerical) / abs(grad_numerical + grad_analytic)
 
-            if rel_error > 0.01:
-                print ('WARNING %f, %f => %e ' % (grad_numerical, grad_analytic, rel_error))
+            if ((grad_numerical + grad_analytic != 0.0) ): # not seen characters have the gradient 0 which causes a division by zero
+                rel_error = abs(grad_analytic - grad_numerical) / abs(grad_numerical + grad_analytic)
+
+                if rel_error > 0.01:
+                    print ('WARNING %f, %f => %e ' % (grad_numerical, grad_analytic, rel_error), "current weight index is ", i)
+                    errorSum += 1
+            else:
+                zeroDivCounter += 1
+        if (zeroDivCounter > 0):
+            print(zeroDivCounter, " divisions by zero skipped")
+    print((errorSum / weightSum)*100, "% of all weights had a higher relative error than 0.01.")
